5d0d4494bf162347130e8782295a71fd793676a2
incub4t0r <dchung0812@gmail.com>
2023-08-27 23:57:58 -0400

    added sunday weekly cronjob to check passkey for slingingai

--- index.js
+++ index.js
@@ -28,67 +28,40 @@ for (const file of commandFiles) {
 	}
 }
 
+const API_URL = "http://localhost:5005/api/passcode"
 
-function calculateDays(){
-    var toSend = ``;
-    var now = dayjs();
-    var grad = dayjs('2023-05-27').add('12', 'hour');
-    const arrivalDate = dayjs('2019-07-01');
-    const totalDays = grad.diff(arrivalDate, 'days');
-    const finalDiff = grad.diff(now);
-    const days = Math.floor(finalDiff / (24 * 60 * 60 * 1000));
-    const hours = Math.floor((finalDiff % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
-    const minutes = Math.floor((finalDiff % (60 * 60 * 1000)) / (60 * 1000));
-    const seconds = Math.floor((finalDiff % (60 * 1000)) / 1000);
-    const formattedDiff = `**${days} days**, **${hours} hours**, **${minutes} minutes**, and **${seconds} seconds**`;
-    const daysSinceArrival = now.diff(arrivalDate, 'days');
-    const sentencePercentage = ((daysSinceArrival/totalDays)*100).toFixed(2);
-    
-    toSend += "🥳 The Class of 2023 has served **" + sentencePercentage + "%** of our total time.";
-    toSend += "\n";
-    toSend += "🅿️ **" + daysSinceArrival + "** days have passed since our arrival on 01 Jul 2019.";
-    toSend += "\n";
-    toSend += `⏳ There are ${formattedDiff} left until graduation on 27 May 2023.`;
-    return toSend;
-}
-
-const graduation = dayjs('2023-05-27');
-
-function dailyMessage(){
-    const now = dayjs().format('DD MMM YYYY');
-    if (graduation.isSame(now,'day')){
-        console.log("Graduation day!");
-        const calculated = calculateDays();
-        client.channels.fetch(channelID).then((channel) => {
-            var toSend = "@everyone";
-            toSend += "\n\n";
-            toSend += `🎓 Congratulations Class of 2023! Today is ${now}.\n`;
-            toSend += "🥳 We made it! Today, we are officially graduating from the United States Military Academy.\n\n";
-            toSend += "It has been an honor serving you all, updating you on your time left here.\n";
-            toSend += "No matter where we go or what we do after graduation, please remember that you are loved and that you have friends.\n\n";
-            toSend += "This is my final countdown message to you all as a class.\n\n";
-            channel.send(toSend + calculated);
-        }); 
+function check_slingingai_passcode(){
+    var final_passcode = "";
+    try {
+        const response = fetch(API_URL, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `${process.env.SHARED_LOCALHOST_SECRET}`,
+            }
+        });
+        const passcode_response = response.text();
+        const passcode_response_parsed = JSON.parse(passcode_response);
+        final_passcode = passcode_response_parsed.passcode.trim();
+    } catch (e) {
+        console.error("Error: ", e);
+        throw e;
     }
-    else if (graduation.isAfter(now,'day')){
-        const calculated = calculateDays();
-        client.channels.fetch(channelID).then((channel) => {
-            var toSend = "@everyone\n\n";
-            toSend += `☀️ Good morning code slingers! Today is ${now}.\n`;
-            channel.send(toSend + calculated);
-        }); 
-    }
-    else {
-        console.log("Graduation has passed");
-    }
+    client.channels.fetch(channelID).then((channel) => {
+        var toSend = "";
+        toSend += "🔑 The passcode for this week is: **";
+        toSend += final_passcode;
+        toSend += "**";
+        channel.send(toSend);
+    });
 }
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
-    cron.schedule('0 12 * * *', () => {
-        dailyMessage();
+    cron.schedule('0 12 * * 0', () => {
+        check_slingingai_passcode();
     });
     console.log("Starting cron job");
 });
